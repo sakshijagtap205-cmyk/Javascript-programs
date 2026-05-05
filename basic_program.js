a19b1ac0412dc7de900ec042d9e0a562a317ccc0
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-05-05 13:11:57 +0530

    Add basic JavaScript program with greeting and addition

---
+++ basic_program.js
@@ -0,0 +1,25 @@
+
+console.log("Hello, Welcome to JavaScript!");
+
+// Declare variables
+let name = "Sakshi";
+let age = 21;
+
+// Display variables
+console.log("My name is " + name);
+console.log("I am " + age + " years old.");
+
+// Simple function
+function greet(userName) {
+    console.log("Hello, " + userName + "!");
+}
+
+// Call function
+greet(name);
+
+// Simple addition
+let a = 10;
+let b = 5;
+let sum = a + b;
+
+console.log("Sum of " + a + " and " + b + " is: " + sum);
