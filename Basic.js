de1695cd7fafac17282529293cbb8c0491df17ad
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-05-05 13:13:51 +0530

    Add Basic.js with greeting and user info

---
+++ Basic.js
@@ -0,0 +1,14 @@
+console.log("Hello, JavaScript!");
+
+let name = "Sakshi";
+let age = 21;
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+
+function greet() 
+{
+    console.log("Welcome to JavaScript Programming!");
+}
+
+greet();
