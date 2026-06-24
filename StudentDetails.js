bbe2a32fdc2bd87e0ecf0b718c350f7388f0b535
sakshi Jagtap <sakshijagtap205@gmail.com>
2026-06-24 23:11:48 +0530

    Create StudentDetails.js

---
+++ StudentDetails.js
@@ -0,0 +1,9 @@
+
+
+let studentName = "Sakshi Jagtap";
+let rollNo = 101;
+let branch = "Computer Engineering";
+
+console.log("Student Name: " + studentName);
+console.log("Roll No: " + rollNo);
+console.log("Branch: " + branch);
